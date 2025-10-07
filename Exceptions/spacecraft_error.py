701b74f887a3c839ee1b349b24ae347286610ace
umar muhaimin <umarmuhaiminbusiness@gmail.com>
2025-10-07 22:41:51 +0800

    update

--- Exceptions/spacecraft_error.py
+++ Exceptions/spacecraft_error.py
@@ -20,3 +20,38 @@ main()
 
 # Output : 
 # → Error : TypeError: can't multiply sequence by non-int of type 'str'
+
+# 👉 Solution : 
+
+distances = { 
+    "Voyager 1": "163",
+    "Voyager 2": "136",
+    "Pioneer 10": "88 AU",
+    "New Horizons": "58",
+    "Pioneer 11": "44 AU"
+}
+
+def main():
+    spacecraft = input("Enter a spacecraft: ")
+
+    try:
+        au = float(distances[spacecraft])
+    except KeyError:
+        print(f"'{spacecraft}' is not in distances dictionary.")
+        return
+    except ValueError:
+        print(f"Can't convert '{distances[spacecraft]}' to a float.")
+        return
+         
+    m = convert(au)
+    print(f"{m} m away")
+
+def convert(au):
+    return au * 149597870700
+
+main()
+
+# → Input : Voyager 1 | Output : 24393037861000.0 m away
+# → Input : Pioneer 10 | Output : Can't convert '88 AU' to a float.
+# → Input : Voyager 3 | Output : 'Voyager 3' is not in distances dictionary.
+
